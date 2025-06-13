e8a6f60062ae2cc6b17f01800d9a0a421260e6bc
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-06-13 00:22:20 -0600

    ADD example of usage of basic module

---
+++ CallingCFromPython/01_Example_Of_C_API/Example.py
@@ -0,0 +1,8 @@
+import example_module
+
+"""
+The import and calling of methods not change, 
+is like another Python module
+"""
+example_module.hello_world('Hello World')
+example_module.hello_world('!!!!!!!!!!!')
