5c650f6dbbdfdb6acfa4a6837583a90b99994c2f
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-07-04 23:19:12 -0600

    MOD section about loading data for adding about dataloader implementation and minor changes for transformations

--- DeepLearning/01_IntroPyTorch/01_Fundamentals.py
+++ DeepLearning/01_IntroPyTorch/01_Fundamentals.py
@@ -18,14 +18,14 @@ def _():
     from torch import nn , Tensor
 
     ## Processing and load datasets
-    from torch.utils.data import Dataset , DataLoader
+    from torch.utils.data import Dataset , DataLoader , random_split
 
     ## Module for CV
     from torchvision import datasets, transforms
 
     ## Additional utils
     from typing import Any
-    return Any, Dataset, Tensor, mo, nn, pd
+    return Any, DataLoader, Dataset, Tensor, mo, nn, pd, random_split, torch
 
 
 @app.cell
@@ -91,16 +91,66 @@ def _(Any, Dataset, Tensor, pd):
             instance = self.pd_dataset.iloc[idx]
             instance_x = instance[self.feature_columns]
             label = instance[self.target_column]
-            return self.transform(instance_x) , self.target_transform(label)
+        
+            if self.transform: instance_x = self.transform(instance_x)
+            if self.target_transform: label = self.target_transform(label)
+            return instance_x , label
+    return (ExampleDataset,)
+
+
+@app.cell
+def _(Tensor, pd):
+    # Functions for preprocessing values from dataset
+
+    def TransformationFeature(instance: pd.Series) -> Tensor:
+        for categorical_feature in ['Stage_fear','Drained_after_socializing']:
+            instance[categorical_feature] = int(instance[categorical_feature][0] == 'Y')
+        return Tensor(instance) # Must return a Tensor type
+
+    def TransformationLabel(label: str) -> int:
+        return int(label[0] == 'E') # For Classification problems, return can be a scalar
+    return TransformationFeature, TransformationLabel
+
+
+@app.cell
+def _(ExampleDataset, TransformationFeature, TransformationLabel, pd):
+    # Definition of dataset using the custom dataset subclass
+
+    PATH = './DeepLearning/01_IntroPyTorch/'
+
+    src_data = PATH + 'personality_dataset.csv'
+    _dataset_columns = pd.read_csv(src_data).columns
+    features = _dataset_columns[:-1]
+    target = _dataset_columns[-1]
+
+    dataset = ExampleDataset(
+        src_data,
+        features,
+        target,
+        TransformationFeature,
+        TransformationLabel,
+    )
+    return (dataset,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"`torch.utils.data.DataLoader` enables easy access/interface to the samples/instances in `torch.utils.data.Dataset` during training stage. Retrieves data in batches of `batch_size` size at each epoch or iteration.")
     return
 
 
 @app.cell
-def _():
-    PATH = './DeepLearning/01_IntroPyTorch/'
-    src_data = PATH + 'personality_dataset.csv'
+def _(DataLoader, dataset, random_split, torch):
+    # Spliting dataset into train and test sets
 
+    rand_gen = torch.random.manual_seed(8013)
+    dataset_train , dataset_test = random_split(dataset,[0.8,0.2],rand_gen)
 
+    # Definition and init of dataloaders
+
+    BATCH_SIZE = 32
+    dataloader_train = DataLoader(dataset_train,batch_size=BATCH_SIZE,shuffle=True)
+    dataloader_test = DataLoader(dataset_test,batch_size=BATCH_SIZE,shuffle=True)
     return
 
 
