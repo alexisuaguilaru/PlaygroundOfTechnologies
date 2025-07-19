ff0ef5e5d2e84c05e9a52a27a2d0fca362719c18
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-07-19 11:25:31 -0600

    ADD section about model training

--- DeepLearning/02_CNN/02_CNN.py
+++ DeepLearning/02_CNN/02_CNN.py
@@ -26,7 +26,7 @@ def _(torch):
 
     TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     TORCH_DEVICE
-    return
+    return (TORCH_DEVICE,)
 
 
 @app.cell
@@ -73,10 +73,10 @@ def _(datasets, transforms):
 def _(DataLoader, dataset_test, dataset_train):
     # Defining dataloaders for training and testing
 
-    batch_size = 16
+    batch_size = 128
     dataloader_train = DataLoader(dataset_train,batch_size=batch_size,shuffle=True)
     dataloader_test = DataLoader(dataset_test,batch_size=batch_size,shuffle=True)
-    return
+    return batch_size, dataloader_test, dataloader_train
 
 
 @app.cell
@@ -101,14 +101,14 @@ def _(Tensor, nn):
 
             # Defining Convolutional Network
             ConvLayer1 = nn.Sequential(
-                nn.Conv2d(3,5,kernel_size=3),
+                nn.Conv2d(3,9,kernel_size=2),
                 nn.ReLU(),
                 nn.MaxPool2d(2,2),
             )
             ConvLayer2 = nn.Sequential(
-                nn.Conv2d(5,4,kernel_size=2,stride=2),
+                nn.Conv2d(9,6,kernel_size=2,stride=2),
                 nn.ReLU(),
-                nn.AvgPool2d(2,2),
+                nn.MaxPool2d(2,2),
             )
             self.ConvNet = nn.Sequential(
                 ConvLayer1,
@@ -118,9 +118,11 @@ def _(Tensor, nn):
 
             # Defining Dense Network
             self.DenseNet = nn.Sequential(
-                nn.Linear(36,12),
+                nn.Linear(54,128),
                 nn.Tanh(),
-                nn.Linear(12,10),
+                nn.Linear(128,64),
+                nn.ReLU(),
+                nn.Linear(64,10),
             )
 
         def forward(
@@ -134,12 +136,100 @@ def _(Tensor, nn):
 
 
 @app.cell
-def _(CNN):
+def _(CNN, TORCH_DEVICE):
     # Init of model
 
-    ModelCNN = CNN()
+    ModelCNN = CNN().to(TORCH_DEVICE)
 
     ModelCNN
+    return (ModelCNN,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"# 3. Model Training")
+    return
+
+
+@app.cell
+def _(TORCH_DEVICE, batch_size, torch):
+    # Based on :: https://docs.pytorch.org/tutorials/beginner/basics/optimization_tutorial.html#optimization-loop
+
+    def train_loop(
+            dataloader, 
+            model,
+            loss_fn,
+            optimizer,
+        ):
+
+        size = len(dataloader.dataset)
+
+        model.train()
+        for batch , data_train in enumerate(dataloader):
+            X , y = data_train[0].to(TORCH_DEVICE) , data_train[1].to(TORCH_DEVICE)
+
+            pred = model(X)
+            loss = loss_fn(pred, y)
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch % 10 == 0:
+                loss, current = loss.item(), batch * batch_size + len(X)
+                print(f"loss: {loss:>5f}  [{current:>10d}/{size:>10d}]")
+
+    def test_loop(dataloader, model, loss_fn):
+        model.eval()
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for data_test in dataloader:
+                X , y = data_test[0].to(TORCH_DEVICE) , data_test[1].to(TORCH_DEVICE)
+
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loop, train_loop
+
+
+@app.cell
+def _(ModelCNN, nn, torch):
+    # Init loss function and optimizer
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    learning_rate = 10e-5
+    optimizer = torch.optim.Adam(
+        ModelCNN.parameters(),
+        lr=learning_rate,
+    )
+    return loss_fn, optimizer
+
+
+@app.cell
+def _(
+    ModelCNN,
+    dataloader_test,
+    dataloader_train,
+    loss_fn,
+    optimizer,
+    test_loop,
+    train_loop,
+):
+    # Training the model/CNN
+
+    epochs = 3
+    for _t in range(epochs):
+        print(f"Epoch {_t+1}\n-------------------------------")
+        train_loop(dataloader_train, ModelCNN, loss_fn, optimizer)
+        test_loop(dataloader_test, ModelCNN, loss_fn)
     return
 
 
