184045518930b0f2de2a4079dc451c2b38f21129
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-05-02 23:45:11 -0600

    ADD example of how to use the invocations endpoint of a local deployed model

---
+++ MLFlow/04_ExampleRequest.py
@@ -0,0 +1,24 @@
+## Basic imports
+import requests
+import json
+import pandas as pd
+
+
+## Load data
+InferenceData = pd.read_csv('./MLDatasets/valid_X.csv')
+
+
+## Request body
+Endpoint = 'http://localhost:5001/invocations'
+Headers = {'Content-Type': 'application/json'}
+DataToPredict = InferenceData.to_dict(orient='split')
+
+
+## Response 
+PredictedResponse = requests.post(
+    Endpoint,
+    headers = Headers,
+    json = {"dataframe_split": DataToPredict},
+)
+
+print(json.loads(PredictedResponse.content))
